7b149ef740ee1900a9adfd4a34b28fd274d66f71
nexus1010 <bot.developer3@gmail.com>
2022-07-16 13:12:47 -0500

    Added links

--- js/handle-scroll.js
+++ js/handle-scroll.js
@@ -1,10 +1,22 @@
 window.onscroll = () => { scrollNavbar() }
+let navbarOpen = false;
 
 scrollNavbar = () => {
     const navbar = document.getElementById("navbar");
-    if (document.documentElement.scrollTop > 100) {
+    if (document.documentElement.scrollTop > 0) {
         navbar.classList.add("scrolled");
     } else {
-        navbar.classList.remove("scrolled");
+        if (!navbarOpen) {
+            navbar.classList.remove("scrolled");
+        }
+    }
+}
+
+const navbarButton = document.getElementById("menu-btn")
+
+navbarButton.onclick = () => {
+    navbarOpen = !navbarOpen;
+    if (document.documentElement.scrollTop <= 0) {
+        navbar.classList.toggle("scrolled");
     }
 }
